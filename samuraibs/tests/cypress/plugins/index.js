b940ee38cec0b8fd2f798af6cd45fc58cd401a48
Miguel Bianchi Dias <mbianchid.12@gmail.com>
2024-03-27 23:41:39 -0300

    uCypress feat: add db config on cypress.json file

--- samuraibs/tests/cypress/plugins/index.js
+++ samuraibs/tests/cypress/plugins/index.js
@@ -23,13 +23,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'ziggy.db.elephantsql.com',
-    user: 'ojueajnt',
-    password: 'hSrQdwfGmKKeLUuKE-N3jaKihaA2w7it',
-    database: 'ojueajnt',
-    port: 5432
-  })
+  const configJson = require(config.configFile)
+
+  const pool = new Pool(configJson.dbConfig)
 
   on('task', {
     removeUser(email){
